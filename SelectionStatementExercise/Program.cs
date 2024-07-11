79d713a9fe51078d7f656f55ef87cda3a20afa8a
CherieMillan <cherie99millan@gmail.com>
2024-07-11 11:51:32 -0400

    Used if and else statements to guess the correct number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("PLease guess a number from 1-100");
+            int favNum = 99;
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            
+            if (userInput < 99)
+            {
+                Console.WriteLine("Number to low");
+            }
+            else if (userInput > 99)
+            {
+                Console.WriteLine("Number too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!");
+            
+            }
         }
     }
 }
